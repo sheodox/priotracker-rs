f5a98140fe88bf0091089c49163bf61ce3630653
sheodox <imagamer27@msn.com>
2015-10-13 22:32:13 -0500

    added the ability to add items to a list and made typing 'back' at any prompt go up a level

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
     };
 
     loop {
-        let prompt = "What do you want to do?";
+        let prompt = "What do you want to do? (Type \"back\" at any time to return to the previos menu)";
         let choices = vec![
             "1. View priority lists".to_string(),
             "2. Create a new priority".to_string(),
@@ -36,50 +36,64 @@ fn main() {
         let desire = switch_board(prompt, choices);
 
         match desire.as_ref() {
-            "1" => {view_priority_lists(&data);},
-            "2" => {create_priority_list(&data);},
-            _ => {println!("Please enter a valid number!");}
+            "1" => {view_priority_lists(&mut data);},
+            "2" => {create_priority_list(&mut data);},
+            "back" => {break;},
+            _ => {println!("Please enter a valid number!");},
         }
     }
 }
 
-fn view_priority_lists(data: &PrioData) {
+fn view_priority_lists(data: &mut PrioData) {
     if data.lists.len() == 0 {
         println!("You have no lists!");
     }
     else {
-        let mut choices: Vec<String> = vec![];
-        let mut index = 0;
+        loop {
+            let mut choices: Vec<String> = vec![];
+            let mut index = 0;
 
-        for list in &data.lists {
-            index += 1;
-            let mut text = index.to_string();
-            text.push_str(". ");
-            text.push_str(&list.name);
-            choices.push(text);
+            for list in &data.lists {
+                index += 1;
+                let mut text = index.to_string();
+                text.push_str(". ");
+                text.push_str(&list.name);
+                choices.push(text);
+            }
+
+            println!("");
+
+            let prompt = "Which list do you want to view?";
+
+            let desire = switch_board(prompt, choices);
+            if desire == "back" {
+                break;
+            }
+
+            let mut index: usize = desire.parse().unwrap();
+            index -= 1;
+            view_list(&mut data.lists[index]);
         }
-
-        println!("");
-
-        let prompt = "Which list do you want to view?";
-
-        let desire = switch_board(prompt, choices);
-        let mut index: usize = desire.parse().unwrap();
-        index -= 1;
-        view_list(index, &data);
     }
 }
 
-fn view_list(list_index: usize, data: &PrioData) {
-    let list = &data.lists[list_index];
-    println!("\n--{}--", list.name);
+fn view_list(list: &mut PrioList) {
+    loop {
+        println!("\n--{}--", list.name);
 
-    for item in &list.items {
-        println!("{}", item);
+        for item in &list.items {
+            println!("{}", item);
+        }
 
+        println!("\nEnter a new item");
+        let new_item = read_line();
 
+        if new_item == "back" {
+            break;
+        }
+
+        list.add_item(&new_item);
     }
-
 }
 
 fn create_priority_list(data: &PrioData) {
@@ -87,16 +101,22 @@ fn create_priority_list(data: &PrioData) {
 }
 
 fn switch_board(prompt: &str, choices: Vec<String>) -> String {
-    println!("{}", prompt);
+    println!("\n{}", prompt);
     for choice in choices {
         println!("{}", choice);
     }
 
-    let mut choice = String::new();
+    let choice = read_line();
 
-    io::stdin().read_line(&mut choice)
+    choice.to_string()
+}
+
+fn read_line() -> String{
+    let mut line = String::new();
+
+    io::stdin().read_line(&mut line)
         .ok()
-        .expect("Failed to read line.");
+        .expect("Failed to read line");
 
-    choice.trim().to_string()
+    line.trim().to_string()
 }
